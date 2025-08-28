4b26531b4f2c72406f5d124fafda08ac6c47c357
NICOLAS <ncollazos@unimayor.edu.co>
2025-08-28 14:43:25 -0500

    Version

--- 01-bases-react/src/App.tsx
+++ 01-bases-react/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
     <BasicTypes/>
     </>
-  )
+  ) 
 }
 
 export default App
--- 01-bases-react/src/Component/BasicTypes.tsx
+++ 01-bases-react/src/Component/BasicTypes.tsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const BasicTypes = () => {
-   const name: string = "Nicolas";
-   const age: number = 20;
-   let isActive: boolean = true;
+   let name: string = "Nicolas"; //Variable //let para poder modificar
+   //const age: number = 20; // Variable de estado
+   const [age, setAge] = useState(28); //useStateSnipped para variable de estado
+   //dar click a ctrl+space+enter para importar libreria
+   //let isActive: boolean = true;
+   const [isActive, setActive] = useState(true);
 
   return (
     <>
-        <div>Hello World</div>
+        <h3>Basic Types</h3>
+        {isActive? //Operador ternario 
+        //si es true:
+        <ul>
+            <li>Nombre: {name}</li>
+            <li>Edad: {age}</li>
+        </ul>/*para el false dos puntos*/:
+        <p>Usuario Inactivo</p>
+        }
 
-        <div>
-            {" "+name}
-        </div>
+        <h2>Variable Normal</h2>
+        <button onClick={
+          ()=> {
+            console.log(`El nombre es: ${name}`)
+          }
+          }>Dar Nombre</button>
 
-        <div>BasicTypes</div>
- 
-        <div>
-            {" "+age}
-        </div>
+        <h2>Variable de Estado Age</h2>
+        <button onClick={
+          ()=> {
+            setAge(29);
+            console.log(`El valor de age es: ${age}`)
+          }
+          }>Modificar Edad</button>
 
-        <div>
-            {" "+isActive}
-        </div>
+        <h2>Variable de Estado Boleano</h2>
+        <button onClick={
+          ()=> {
+            setActive(!isActive);
+            console.log(`El usuario esta: ${isActive}`)
+          }
+          }>Modificar Estado</button>
     </>
 
   )
